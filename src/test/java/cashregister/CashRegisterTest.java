fbec20d0d4df6f988492fdfca9ea9552776b711d
GUKSON LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUGU2@OOCL.COM>
2019-07-15 16:45:05 +0800

    [LJC]complete real print and mock print

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,25 +1,51 @@
 package cashregister;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
 
 
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setup() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
+
     @Test
     public void should_print_the_real_purchase_when_call_process() {
-        //given
-        //when
-        //then
+        Printer printer = new Printer();
+        Item[] items = {new Item("coco", 1.0)};
+        CashRegister cashRegister = new CashRegister(printer);
+        Purchase purchase = new Purchase(items);
+
+        cashRegister.process(purchase);
+
+        assertThat(systemOut()).isEqualTo("coco\t1.0\n");
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
-        //given
-        //when
-        //then
+        Printer printer = new Printer();
+        CashRegister cashRegister = new CashRegister(printer);
+        Purchase purchase = mock(Purchase.class);
+        when(purchase.asString()).thenReturn("mock purchase");
+
+        cashRegister.process(purchase);
+
+        assertThat(systemOut()).isEqualTo("mock purchase");
     }
 
     @Test
